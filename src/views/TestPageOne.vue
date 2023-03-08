91fd7abb7c5feb30d6463815b4fbf2225578a51a
chaoyue <fangzchao@outlook.com>
2023-03-08 21:21:38 +0800

    💄 build UI style library（padding、margin、font、top/left）

--- src/views/TestPageOne.vue
+++ src/views/TestPageOne.vue
@@ -1,7 +1,25 @@
 <template>
-  <h1>hello world I am TestPageOne</h1>
+  <h1>实现一个flex的布局</h1>
+  <div class="box">
+    <div class="left">1</div>
+    <div class="center">2</div>
+    <div class="right">3</div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.box {
+  display: flex;
+}
+.left {
+  flex-grow: 1;
+}
+.center {
+  flex-grow: 2;
+}
+.right {
+  flex-grow: 1;
+}
+</style>
--- src/views/TestPageTwo.vue
+++ src/views/TestPageTwo.vue
@@ -1,5 +1,6 @@
 <template>
-  <h1>hello world I am TestPageTwo</h1>
+  <h1>解析URL提取params参数</h1>
+  <h1 class="g-m10">解析URL提取params参数</h1>
 </template>
 
 <script setup lang="ts"></script>
