f10db16f05b811fe108ecd455affa68706563d93
chaoyue <fangzchao@outlook.com>
2023-04-03 22:18:57 +0800

    🐛 the function parameter should be passed to the throttle

--- src/libs/throttle.ts
+++ src/libs/throttle.ts
@@ -1,9 +1,13 @@
 import { getCurrentInstance } from 'vue';
-export function throttle(func: () => void, wait: number): () => void {
+
+export function throttle(
+  func: (...args: any[]) => void,
+  wait: number
+): () => void {
   let timer: number | null;
   const instance = getCurrentInstance();
 
-  return function (...args) {
+  return function (...args: any[]) {
     if (!timer) {
       timer = setTimeout(function () {
         timer = null;
