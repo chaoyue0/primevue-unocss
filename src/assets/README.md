b4a1a0b064a9c3b66ff10453682f2ad593a26423
chaoyue <fangzchao@outlook.com>
2023-04-12 17:52:35 +0800

    💄 build flex style library

---
+++ src/assets/README.md
@@ -0,0 +1,10 @@
+## BEM命名约定
+### 定义
+对 css 命名的一种规范，将页面模块化，隔离样式，提高代码的复用性，减少后期的维护成本；
+- block：表示更高级别的组件或块级
+- element：表示block的后代元素
+- modifier：表示block的不同状态或不同版本
+
+### 书写规范
+- block与element之间用双下划线连接-
+- element与modifier之间用单下划线连接_
---
+++ src/views/TestPageThree.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>flex布局</h1>
+  <p>水平方向</p>
+  <el-button @click="alignX = 'left'">左对齐</el-button>
+  <el-button @click="alignX = 'center'">居中</el-button>
+  <el-button @click="alignX = 'right'">右对齐</el-button>
+  <p>垂直方向</p>
+  <el-button @click="alignY = 'left'">左对齐</el-button>
+  <el-button @click="alignY = 'center'">居中</el-button>
+  <el-button @click="alignY = 'right'">右对齐</el-button>
+  <div class="f-all_center">
+    <div :class="['demo-box', 'f-main_' + alignX, 'f-align_' + alignY]">
+      <div v-for="num in 3" :key="num">
+        {{ num }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+const alignX = ref('left');
+const alignY = ref('left');
+</script>
+
+<style scoped>
+.demo-box {
+  width: 400px;
+  height: 400px;
+  background-color: #b7b7b7;
+}
+.demo-box div {
+  width: 50px;
+  height: 50px;
+  background-color: #5992b6;
+}
+</style>
