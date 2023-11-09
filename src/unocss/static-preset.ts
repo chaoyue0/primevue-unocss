d85365a002c157e3d4035575fc63dcabdb79d2dd
chaoyue <fangzchao@outlook.com>
2023-11-09 16:24:13 +0800

    :sparkles: supplement static-preset

--- src/unocss/static-preset.ts
+++ src/unocss/static-preset.ts
@@ -3,7 +3,14 @@ import type { Preset, Rule } from 'unocss';
 export default function staticPreset(): Preset {
   return {
     name: 'static-preset',
-    rules: [...appearances, ...resizes, ...userSelects],
+    rules: [
+      ...appearances,
+      ...resizes,
+      ...userSelects,
+      ...breaks,
+      ...textWraps,
+      ...textTransforms,
+    ],
   };
 }
 
@@ -26,4 +33,20 @@ const userSelects: Rule[] = [
   ['select-all', { '-webkit-user-select': 'all', 'user-select': 'all' }],
   ['select-text', { '-webkit-user-select': 'text', 'user-select': 'text' }],
   ['select-none', { '-webkit-user-select': 'none', 'user-select': 'none' }],
+];
+
+const breaks: Rule[] = [
+  ['break-all', { 'word-break': 'break-all' }],
+  ['break-keep', { 'word-break': 'keep-all' }],
+];
+
+const textWraps: Rule[] = [
+  ['wrap-words', { 'overflow-wrap': 'break-word' }],
+  ['wrap-anywhere', { 'overflow-wrap': 'anywhere' }],
+];
+
+const textTransforms: Rule[] = [
+  ['case-upper', { 'text-transform': 'uppercase' }],
+  ['case-lower', { 'text-transform': 'lowercase' }],
+  ['case-capital', { 'text-transform': 'capitalize' }],
 ];
