a8b4a86903423eb7203dc4304dd7bec4b01bff01
chaoyue <fangzchao@outlook.com>
2024-01-05 15:17:45 +0800

    ➕ add preset-uno

--- uno.config.ts
+++ uno.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'unocss';
 import flexPreset from './src/unocss/flex-preset';
 import gridPreset from './src/unocss/grid-preset';
 import layoutPreset from './src/unocss/layout-preset';
+import presetUno from '@unocss/preset-uno';
 
 export default defineConfig({
-  presets: [flexPreset(), gridPreset(), layoutPreset()],
+  presets: [presetUno, flexPreset(), gridPreset(), layoutPreset()],
 });
