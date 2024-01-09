446ba645b59b775a63051ef95166472233b8f06a
chaoyue <fangzchao@outlook.com>
2024-01-09 14:19:35 +0800

    ✨ add custom made

--- src/MainView.vue
+++ src/MainView.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="f-flex">
-    <PrimeMenu @do-toggle="doToggle"></PrimeMenu>
-    <div class="layout-main">
-      <AppTopBar></AppTopBar>
-      <div class="layout-content" :class="isToggle ? '' : 'ml15.5'">
+    <PrimeMenu
+      v-show="menuType === 'Sidebar'"
+      @do-toggle="doToggle"
+    ></PrimeMenu>
+    <div
+      class="layout-main"
+      :style="menuType === 'Horizontal' ? 'padding-top: unset' : ''"
+    >
+      <PrimeMenuBar v-show="menuType === 'Horizontal'"></PrimeMenuBar>
+      <div
+        class="layout-content"
+        :class="isToggle || menuType === 'Horizontal' ? '' : 'ml15.5'"
+      >
         <router-view v-slot="{ Component, route }">
           <keep-alive :max="30">
             <component :is="Component" :key="route.fullPath" />
@@ -12,15 +21,23 @@
       </div>
     </div>
   </div>
+  <CustomMade @menu-type="getMenuType"></CustomMade>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
-import AppTopBar from '@/components/layout/AppTopBar.vue';
 import PrimeMenu from '@/components/menu/PrimeMenu.vue';
 import { ref } from 'vue';
+import PrimeMenuBar from '@/components/menu/PrimeMenuBar.vue';
+import CustomMade from '@/components/custom/CustomMade.vue';
 
 const isToggle = ref(false);
+const menuType = ref('');
+
+const getMenuType = (s: string) => {
+  menuType.value = s;
+};
+
 const doToggle = () => {
   isToggle.value = !isToggle.value;
 };
---
+++ src/components/custom/CustomMade.vue
@@ -0,0 +1,59 @@
+<template>
+  <Button
+    icon="pi pi-spin pi-cog"
+    class="fixed right-0 top-50"
+    @click="sidebarVisible = true"
+  ></Button>
+  <Sidebar
+    v-model:visible="sidebarVisible"
+    :show-close-icon="false"
+    position="right"
+    header="Custom Made"
+  >
+    <p class="font-size-5 font-500">Menu Type</p>
+    <div class="mb">
+      <RadioButton
+        v-model="menuType"
+        input-id="ingredient1"
+        name="menu"
+        value="Horizontal"
+        @change="changeRadio"
+      />
+      <label for="ingredient1" class="ml-2">Horizontal</label>
+    </div>
+    <div class="mb">
+      <RadioButton
+        v-model="menuType"
+        input-id="ingredient2"
+        name="menu"
+        value="Sidebar"
+        @change="changeRadio"
+      />
+      <label for="ingredient2" class="ml-2">Sidebar</label>
+    </div>
+    <Divider />
+  </Sidebar>
+</template>
+
+<script setup lang="ts">
+import RadioButton from 'primevue/radiobutton';
+import Divider from 'primevue/divider';
+import Button from 'primevue/button';
+import Sidebar from 'primevue/sidebar';
+import { onMounted, ref } from 'vue';
+
+const sidebarVisible = ref(false);
+const menuType = ref('Sidebar');
+
+const emit = defineEmits<{ (e: 'menu-type', menuType: string): void }>();
+
+onMounted(() => {
+  emit('menu-type', menuType.value);
+});
+
+const changeRadio = () => {
+  emit('menu-type', menuType.value);
+};
+</script>
+
+<style scoped></style>
